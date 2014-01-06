def94c2b91cf68e4cc84e8c2db4727bf556d201f
tomholub <info@nvimp.com>
2014-01-06 09:24:40 -0800

    fixed bug in Texts library

--- www/app/libs/Texts.php
+++ www/app/libs/Texts.php
@@ -15,12 +15,12 @@ class Texts extends \Nette\Object{
 	private $data = Array();
 	
 	public function __construct() {
-		$data['LogMeException'] = Array(
+		$this->data['LogMeException'] = Array(
 			10	=> 'Unable to recover expired Coinbase token',
 			11	=> 'Unable to connect to Coinbase',
 			12	=> 'Problem with Coinbase API',
 		);
-		$data['CoinbaseErrors'] = Array(
+		$this->data['CoinbaseErrors'] = Array(
 			'first_purchase' => 'Please wait until your first bitcoin purchase completes before making additional purchases., (Once your first purchase completes you can make multiple purchases at the same time.)',
 		);
 	}
