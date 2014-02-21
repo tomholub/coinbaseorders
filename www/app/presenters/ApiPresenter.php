69899f7889bd26ca80ed9c1ddf5d6c0e45b0c8e4
tomholub <info@nvimp.com>
2014-02-20 18:27:14 -0800

    only try to process order if users API tokens are set

--- www/app/presenters/ApiPresenter.php
+++ www/app/presenters/ApiPresenter.php
@@ -42,13 +42,15 @@ class ApiPresenter extends BasePresenter {
 	public function checkBuyOrders($currentBuyPrice) {
 		$sqlWhere = Array('status' => 'ACTIVE', 'action' => 'BUY', "$currentBuyPrice <= `at_price`");
 		foreach ($this->context->orders->findAll()->where($sqlWhere) as $order) { //double check the price for each before buying
-			$totalBuyPrice = $this->context->coinbase->user($order->user_id)->order($order)->getBuyPrice($order->amount);
-			if ($totalBuyPrice !== NULL && $totalBuyPrice->subtotal->amount <= $order->at_price * $order->amount) {
-				$result = $this->context->coinbase->user($order->user_id)->order($order)->buy($order->amount); //Buy the coins
-				$this->context->orders->findAll()->get($order->order_id)->update(Array('status' => 'EXECUTED')); //Update order status
-				
-				$user = $this->context->authenticator->getUser($order->user_id);
-				new SendEmail($user->email, 'You just bought Bitcoin using limit order on Coinbase!', 'Hi there!<br/><br/>The system just executed your order to buy Bitcoin. You can check the details at <a href="http://coinbaseorders.com/">http://coinbaseorders.com/</a>.<br/><br/>Coinbase Orders is a free service. Please consider a small donation, others have donated too. The donation address is 13ejFczTyMsdZQHkrfVEfiGY8RLD2rDs9i, alternatively <a href="http://coinbaseorders.com/homepage/donate">click here to get donation QR code</a>.<br/><br/>I appriciate your help!<br/><br/>Tom');				
+			$userAssociatedWithOrder = $this->context->authenticator->getUser($order->user_id);
+			if($userAssociatedWithOrder->coinbase_access_token){ //his Coinbase API tokens are set
+				$totalBuyPrice = $this->context->coinbase->user($order->user_id)->order($order)->getBuyPrice($order->amount);
+				if ($totalBuyPrice !== NULL && $totalBuyPrice->subtotal->amount <= $order->at_price * $order->amount) {
+					$result = $this->context->coinbase->user($order->user_id)->order($order)->buy($order->amount); //Buy the coins
+					$this->context->orders->findAll()->get($order->order_id)->update(Array('status' => 'EXECUTED')); //Update order status
+					
+					new SendEmail($userAssociatedWithOrder->email, 'You just bought Bitcoin using limit order on Coinbase!', 'Hi there!<br/><br/>The system just executed your order to buy Bitcoin. You can check the details at <a href="http://coinbaseorders.com/">http://coinbaseorders.com/</a>.<br/><br/>Coinbase Orders is a free service. Please consider a small donation, others have donated too. The donation address is 13ejFczTyMsdZQHkrfVEfiGY8RLD2rDs9i, alternatively <a href="http://coinbaseorders.com/homepage/donate">click here to get donation QR code</a>.<br/><br/>I appriciate your help!<br/><br/>Tom');				
+				}				
 			}
 		}
 	}
@@ -56,13 +58,15 @@ class ApiPresenter extends BasePresenter {
 	public function checkSellOrders($currentSellPrice) {
 		$sqlWhere = Array('status' => 'ACTIVE', 'action' => 'SELL', "$currentSellPrice >= `at_price`");
 		foreach ($this->context->orders->findAll()->where($sqlWhere) as $order) { //double check the price for each before selling
-			$totalSellPrice = $this->context->coinbase->user($order->user_id)->order($order)->getSellPrice($order->amount);
-			if ($totalSellPrice !== NULL && $totalSellPrice->subtotal->amount >= $order->at_price * $order->amount) {
-				$result = $this->context->coinbase->user($order->user_id)->order($order)->sell($order->amount); //Sell the coins
-				$this->context->orders->findAll()->get($order->order_id)->update(Array('status' => 'EXECUTED')); //Update order status
-				
-				$user = $this->context->authenticator->getUser($order->user_id);
-				new SendEmail($user->email, 'You just sold Bitcoin using limit order on Coinbase!', 'Hi there!<br/><br/>The system just executed your order to buy Bitcoin. You can check the details at <a href="http://coinbaseorders.com/">http://coinbaseorders.com/</a>.<br/><br/>Coinbase Orders is a free service. Please consider a small donation, others have donated too. The donation address is 13ejFczTyMsdZQHkrfVEfiGY8RLD2rDs9i, alternatively <a href="http://coinbaseorders.com/homepage/donate">click here to get donation QR code</a>.<br/><br/>I appriciate your help!<br/><br/>Tom');
+			$userAssociatedWithOrder = $this->context->authenticator->getUser($order->user_id);
+			if($userAssociatedWithOrder->coinbase_access_token){ //his Coinbase API tokens are set			
+				$totalSellPrice = $this->context->coinbase->user($order->user_id)->order($order)->getSellPrice($order->amount);
+				if ($totalSellPrice !== NULL && $totalSellPrice->subtotal->amount >= $order->at_price * $order->amount) {
+					$result = $this->context->coinbase->user($order->user_id)->order($order)->sell($order->amount); //Sell the coins
+					$this->context->orders->findAll()->get($order->order_id)->update(Array('status' => 'EXECUTED')); //Update order status
+
+					new SendEmail($userAssociatedWithOrder->email, 'You just sold Bitcoin using limit order on Coinbase!', 'Hi there!<br/><br/>The system just executed your order to buy Bitcoin. You can check the details at <a href="http://coinbaseorders.com/">http://coinbaseorders.com/</a>.<br/><br/>Coinbase Orders is a free service. Please consider a small donation, others have donated too. The donation address is 13ejFczTyMsdZQHkrfVEfiGY8RLD2rDs9i, alternatively <a href="http://coinbaseorders.com/homepage/donate">click here to get donation QR code</a>.<br/><br/>I appriciate your help!<br/><br/>Tom');
+				}
 			}
 		}
 	}
